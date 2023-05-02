401454d4cd94f897dd8f82afc84efcf4bb3b0795
Ilse Swanepoel <i.swanepoel19@gmail.com>
2023-05-02 23:33:28 +1200

    attempts at fixing CSS but still broken

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,18 +1,50 @@
+//Script for Hiding and SHowing Nav-bar and page contents. GL
 
-  const menuBtn = document.querySelector(".burger-menu");
-  const navItems = document.querySelector(".navigation-bar");
+const menuBtn = document.querySelector('.burger-menu')
+const navItems = document.querySelector('.display-menu')
+const aboutMe = document.querySelector('.about-me')
+const nameh1 = document.querySelectorAll('.nameh1')
+const nameh2 = document.querySelector('.nameh2')
+const navItemss = document.querySelectorAll('.nav-item')
 
-  let showMenu = false;
-  menuBtn.addEventListener("click", toggleMenu);
+document.addEventListener('DOMContentLoaded', function () {
+  menuBtn.classList.remove('close')
+  menuBtn.classList.add('open')
+  navItems.classList.remove('open')
+  navItems.classList.add('close')
+})
 
-  function toggleMenu() {
-    if (!showMenu) {
-      menuBtn.classList.add("close");
-      navItems.classList.add("show-menu");
-      showMenu = true;
-    } else {
-      menuBtn.classList.remove("close");
-      navItems.classList.remove("show-menu");
-      showMenu = false;
-    }
-  }
+menuBtn.addEventListener('click', toggleMenu)
+navItemss.forEach((navItem) => {
+  navItem.addEventListener('click', toggleBurger)
+})
+
+function toggleMenu() {
+  menuBtn.classList.add('close')
+  menuBtn.classList.remove('open')
+  nameh2.classList.add('close')
+  nameh2.classList.remove('open')
+  nameh1.forEach((item) => {
+    item.classList.add('close')
+    item.classList.remove('open')
+  })
+  aboutMe.classList.add('close')
+  aboutMe.classList.remove('open')
+  navItems.classList.add('open')
+  navItems.classList.remove('close')
+}
+
+function toggleBurger() {
+  menuBtn.classList.remove('close')
+  menuBtn.classList.add('open')
+  nameh2.classList.add('open')
+  nameh2.classList.remove('close')
+  nameh1.forEach((item) => {
+    item.classList.add('open')
+    item.classList.remove('close')
+  })
+  aboutMe.classList.add('open')
+  aboutMe.classList.remove('close')
+  navItems.classList.remove('open')
+  navItems.classList.add('close')
+}
